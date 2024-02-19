89382bd84e433d815e7b1f8dacb405e22d14a854
Chad van Basedman <113964826+redrededredred@users.noreply.github.com>
2024-02-19 10:44:04 +0100

    Added stuff
    stuff the bot needs to work

--- src/bot.nim
+++ src/bot.nim
@@ -1,3 +1,19 @@
 import std/httpclient
+import std/parsecfg
 
 
+const
+  appId: string = ""
+  guildId: string = ""
+  guildCommandEndpoint: string = 
+    "https://discord.com/api/v10/applications/" &
+    appId &
+    "/guilds/" &
+    guildId &
+    "/commands"
+
+let
+  config: Config = loadConfig("bot.ini")
+  token: string = config.getSectionValue("Package", "token")
+
+echo token
