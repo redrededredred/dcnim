c64ebc70b109c922fef15ec0d59846e974e8b15c
Chad van Basedman <113964826+redrededredred@users.noreply.github.com>
2024-02-20 09:58:53 +0100

    Update bot.nim
    Added scuffed sig check

--- src/bot.nim
+++ src/bot.nim
@@ -4,6 +4,7 @@ import std/json
 import htmlgen
 import jester
 import ed25519
+import std/strutils
 
 const
   appId: string = ""
@@ -23,7 +24,12 @@ let
 type
   AppCommandType = enum
     CHAT_INPUT = 1, USER = 2, MESSAGE = 3
+  Bytearray = array[64, byte]
 
+# This is just plain retarded....
+proc StrToByteArray(s: string): Bytearray =
+  for i, b in toOpenArrayByte(s, 0, s.len - 1):
+    result[i] = b
 
 proc addCommand(client: HttpClient, command: JsonNode) {.discardable.} = 
   if client.post(discordEndpoint & appId & "guilds" & guildId & "/commands", body = $command).code == Http200:
@@ -44,8 +50,14 @@ routes:
   get "/":
     resp %*[{ "name": 2, "age": 30 }]
   post "/":
-    let discordJson = parseJson(request.body)
-    if discordJson["type"].getInt == 1:
-      resp.setHeader({"awd": "awd"})
-      resp %*[{"type": 1}]
+    let
+      signatureRecived: string = request.headers["X-Signature-Ed25519"]
+      timestampRecived: string = request.headers["X-Signature-Timestamp"]
+      discordJson: JsonNode = parseJson(request.body)
+      isVerfied: bool = verify(message = timestampRecived & $request.body, signature = Signature(StrToByteArray(signatureRecived)), publicKey = PublicKey(StrToByteArray(pubKey)))
+
+    if not isVerfied:
+      resp(Http401)
+    elif discordJson["type"].getInt == 1:
+      resp(Http200,  {"Access-Control-Allow-Origin":"*"}, $(%*[{"type": 1}]))
   
